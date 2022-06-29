ffcf699c2973b4c5d056b23e56d4cd99ed0bb36e
BlaqLug <lpatrein@gmail.com>
2022-06-29 02:44:06 +0200

    Add cache management for urls

--- backend/models/url_shortner.py
+++ backend/models/url_shortner.py
@@ -1,9 +1,10 @@
 import os
 from fastapi import HTTPException
 
-import uuid #needs to contain letters and integers
+import uuid # contains letters and integers
 import json
 
+#Add urls.json file path
 url_json_path = os.path.abspath("models/urls.json")
 
 
@@ -29,41 +30,45 @@ def write_to_urls(id: str, url: str) -> None:
         json.dump(urls, f)
 
 
-#cache = {}
+cache = {}
 
 
 def encode(url: str) -> str:
-    """DESC: Encodes a URL to a shortened URL
+    """
+    DESC: Encodes a URL to a shortened URL
     INPUT: URL
     OUTPUT: shortened URL
 
     """
-    # encode the url
-    # store the url and the shortened url in a dictionary
+    # encode url 6 characters
     id = str(uuid.uuid3(uuid.NAMESPACE_URL, url))[:6]
-    # urls = get_urls_id(id)
+    
+    # check url in cache
+    if id not in cache:
+        cache[id] = url
 
-    # Removing the cache
-    # if id not in cache:
-    #     cache[id] = url
-    write_to_urls(id=id, url=url)
-    shortened_url = f"https://shorturl.com/{id}"
+        # store the url and the shortened url in a dictionary
+        write_to_urls(id=id, url=url)
+
+    shortened_url = f"https://short.est/{id}"
+
     return shortened_url
 
 
 def decode(url: str) -> str:
-    """DESC: Decodes a shortened URL to its original URL
+    """
+    DESC: Decodes a shortened URL to its original URL
     INPUT: shortened URL
     OUTPUT: original URL
     """
-    # find url in cache
+    
     # break down the url
     id = url.split("/")[-1]
 
-    # removing the cache
-    # print(f"cache: {cache}")
-    # if id in cache:
-    #     return cache[id]
+    # find url in cache
+    print(f"cache: {cache}")
+    if id in cache:
+        return cache[id]
 
     try:
         url = get_url_id(id)
